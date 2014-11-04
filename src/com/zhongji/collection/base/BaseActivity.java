26d6fde7de8d304460efbd6edca1e8ba1fdb8e0c
d76ty76rtf <441245733@qq.com>
2014-11-04 23:20:33 +0800

    deng2

--- src/com/zhongji/collection/base/BaseActivity.java
+++ src/com/zhongji/collection/base/BaseActivity.java
@@ -15,7 +15,7 @@ import com.zhongji.collection.util.ToastUtils;
 public abstract class BaseActivity extends FinalActivity{
 	
 	protected SlidingMenu menu;
-	protected TextView tv_name,abc;  
+	protected TextView tv_name,abc,abcc;  
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
